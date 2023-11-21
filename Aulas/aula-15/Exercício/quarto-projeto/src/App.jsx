6838605dc6aa5568243b779e8978442e22c5dc1a
Anderson A. Silva <andersonadsilva15@gmail.com>
2023-11-20 21:28:56 -0300

    Removendo aulas do frontend

--- Aulas/aula-15/Exercício/quarto-projeto/src/App.jsx
+++
@@ -1,7 +0,0 @@
-import Weather from "./pages/Weather/weather";
-
-const App = () => {
-  return <Weather />
-}
-
-export default App;
--- Aulas/aula-15/Exercício/quarto-projeto/src/components/button/button.jsx
+++
@@ -1,7 +0,0 @@
-import './button.css';
-
-const Button = ({name}) => {
-    return <input type="button">{name}</input>
-}
-
-export default Button;
